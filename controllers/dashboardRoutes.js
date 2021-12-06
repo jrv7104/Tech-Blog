ae5ee17841ec6957fe2ed1ff58d2acce22b15311
Jason Viox <jrv04@hotmail.com>
2021-12-06 11:14:17 -0600

    Start post route for selected user

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id,
+        },
+        
 //     try {
 //       const userData = await User.findAll({
 //         attributes: { exclude: ['password'] },
