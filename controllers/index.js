6af26b412462ec58f98771e0d852a11639f9753f
Jason Viox <jrv04@hotmail.com>
2021-12-10 15:40:59 -0600

    Edit duplicate routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
-
-
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use("/dashboard", dashboardRoutes);
