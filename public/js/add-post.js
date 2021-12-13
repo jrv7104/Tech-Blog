93564a3443e256b39760e11d3e9591b10df6cd78
Jason Viox <jrv04@hotmail.com>
2021-12-13 08:48:51 -0600

    Add const content

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -2,5 +2,5 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    
+    const content = document.querySelector('input[name="content"]').value;
 }
