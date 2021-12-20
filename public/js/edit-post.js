efaf6cadc9ce3175053f33cf18a622c1ef0a074e
Jason Viox <jrv04@hotmail.com>
2021-12-20 16:09:41 -0600

    Add if response ok status, comment out response

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -9,7 +9,23 @@ async function editFormHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
-    const response = await fetch(`/api/posts/${id}`), {
-        
+    // const response = await fetch(`/api/posts/${id}`), {
+    //     method: "PUT",
+    //     body: JSON.stringify({
+    //         post_id: id,
+    //         title,
+    //         content,
+    //     }),
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    // });
+
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        alert(response.statusText);
     }
 }
+
+document.querySelector(".edit-post-form").addEventListener("submit", editFormHandler);
