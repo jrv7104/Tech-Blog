b966ae0400b6d8be0109219c584cc3e0c77832eb
Jason Viox <jrv04@hotmail.com>
2021-12-21 17:30:36 -0600

    Add const password

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,5 +2,7 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username').value.trim;
+    const password = document.querySelector('#password').value.trim;
+
     
 }
